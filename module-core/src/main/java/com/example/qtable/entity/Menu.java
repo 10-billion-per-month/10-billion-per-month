f5568fc2da40d9fa71b7b2999c615ed43d488bb1
gayoung <wjdrkdudwkd@playnomm.com>
2023-06-07 14:54:49 +0900

    #4 close - 'org.springframework.boot' version '3.1.0' | java version '17'

--- module-core/src/main/java/com/example/qtable/entity/Menu.java
+++ module-core/src/main/java/com/example/qtable/entity/Menu.java
@@ -4,10 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
--- module-core/src/main/java/com/example/qtable/entity/Order.java
+++ module-core/src/main/java/com/example/qtable/entity/Order.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
