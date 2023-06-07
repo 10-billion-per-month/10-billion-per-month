6494f2471bb7a3771a8934f61a1851d0d7925ce4
gayoung <wjdrkdudwkd@playnomm.com>
2023-06-07 16:38:01 +0900

    #5 Close - 'api-module' 생성

---
+++ api-module/src/main/java/com/example/apimodule/ApiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.apimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiModuleApplication.class, args);
+    }
+
+}
---
+++ api-module/src/test/java/com/example/apimodule/ApiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
