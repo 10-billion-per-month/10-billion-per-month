4038e1422a56b97e2778e75688628488582aafe9
jeonggayoung <wjdrkdudWkd>
2023-06-16 00:05:03 +0900

    #29 카테고리 목록 조회

--- boot/owner-api/src/test/java/com/example/dev/service/CategoryReadServiceTest.java
+++ boot/owner-api/src/test/java/com/example/dev/service/CategoryReadServiceTest.java
@@ -7,11 +7,14 @@ import com.example.dev.entity.Store;
 import com.example.dev.repository.CategoryRepository;
 import com.example.dev.repository.OwnerRepository;
 import com.example.dev.repository.StoreRepository;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -70,14 +73,27 @@ public class CategoryReadServiceTest {
         Category categoryEntity4 = createCategoryEntity();
 
         // when : 실제 수행
-        Pageable pageable = PageRequest.of(0, 3, Sort.by("storeId").descending());
+        Pageable pageable = PageRequest.of(0, 3, Sort.by("categoryId").descending());
 
         CategoryDto categoryDto = CategoryDto.builder()
                 .storeId(store.getStoreId())
                 .pageable(pageable)
                 .build();
-        categoryReadService.getCategorys(categoryDto);
+        Page<CategoryDto> categorys = categoryReadService.getCategorys(categoryDto);
 
         // then : 수행 결과 확인
+        Assertions.assertThat(categorys)
+                .hasSize(3)
+                .extracting("categoryName", "categoryDescription")
+                .contains(Tuple.tuple(categoryEntity4.getCategoryName(), categoryEntity4.getCategoryDescription())
+                        , Tuple.tuple(categoryEntity3.getCategoryName(), categoryEntity3.getCategoryDescription())
+                        , Tuple.tuple(categoryEntity2.getCategoryName(), categoryEntity2.getCategoryDescription())
+                );
+
+        // 페이징 데이터 확인
+        Assertions.assertThat(categorys.getTotalElements())
+                .isEqualTo(4);
+        Assertions.assertThat(categorys.getTotalPages())
+                .isEqualTo(2);
     }
 }
