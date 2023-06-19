969cc35bde3e9fdbd9664912cf30943290ca88ea
gayoung <wjdrkdudwkd@playnomm.com>
2023-06-19 15:24:34 +0900

    #44 큐알코드 목록 조회

--- boot/owner-api/src/main/java/com/example/dev/controller/QrcodeController.java
+++ boot/owner-api/src/main/java/com/example/dev/controller/QrcodeController.java
@@ -1,11 +1,17 @@
 package com.example.dev.controller;
 
 import com.example.dev.dto.request.CreateQrcodeRequestDto;
+import com.example.dev.dto.request.QrcodesRequestDto;
 import com.example.dev.dto.response.CreateQrcodeResponseDto;
+import com.example.dev.dto.response.QrcodesResponseDto;
 import com.example.dev.service.QrcodeReadService;
 import com.example.dev.service.QrcodeWriteService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +31,16 @@ public class QrcodeController {
     @PostMapping("/v1/qrcode")
     public CreateQrcodeResponseDto createQrcode(@Valid CreateQrcodeRequestDto requestDto) {
         return CreateQrcodeResponseDto.from(qrcodeWriteService.createQrcode(requestDto.toDto()));
+    }
+
+    /**
+     * 가게의 큐알코드 목록 조회
+     * @param requestDto
+     * @param pageable
+     * @return
+     */
+    @GetMapping("/v1/qrcodes")
+    public Page<QrcodesResponseDto> getQrcodes(@Valid QrcodesRequestDto requestDto, @PageableDefault(size = 10, page = 0, sort = "qrcodeId", direction = Sort.Direction.ASC) Pageable pageable) {
+        return qrcodeReadService.getQrcodes(requestDto.toDto(), pageable).map(QrcodesResponseDto::from);
     }
 }
--- boot/owner-api/src/main/java/com/example/dev/service/QrcodeReadService.java
+++ boot/owner-api/src/main/java/com/example/dev/service/QrcodeReadService.java
@@ -1,6 +1,15 @@
 package com.example.dev.service;
 
+import com.example.dev.dto.QrcodeDto;
+import com.example.dev.dto.request.QrcodesRequestDto;
+import com.example.dev.entity.Qrcode;
+import com.example.dev.exception.CommonException;
+import com.example.dev.exception.ErrorCode;
+import com.example.dev.repository.QrcodeRepository;
+import com.example.dev.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,4 +17,21 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class QrcodeReadService {
+
+    private final QrcodeRepository qrcodeRepository;
+    private final StoreRepository storeRepository;
+
+    /**
+     * 가게의 큐알코드 목록 조회
+     * @param requestDto
+     * @param pageable
+     * @return
+     */
+    public Page<QrcodeDto> getQrcodes(QrcodeDto dto, Pageable pageable) {
+        Long storeId = dto.getStoreId();
+        storeRepository.findById(storeId)
+                        .orElseThrow(() -> new CommonException(ErrorCode.INVALID_INPUT_VALUE, String.format("존재하지 않는 가게입니다. storeId = %s", storeId)));
+        return qrcodeRepository.findAllByStore_StoreId(storeId, pageable)
+                .map(QrcodeDto::from);
+    }
 }
